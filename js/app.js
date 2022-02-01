bc23b203e311b958706a66486a7cd0af2b49092f
MohanadLinux <mohanadlinux@gmail.com>
2022-02-01 23:32:12 +0300

    Solving responsive navbar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-const toggleButton = document.getElementsByClassName('hamburger-menu')[0]
-const navbarLinks = document.getElementsByClassName('navigation')[0]
 
-toggleButton.addEventListener('click', () =>{
-    navbarLinks.classList.toggle('active')
-})
+const header = document.getElementById('header');
+const toggle = document.getElementById('toggle');
+const navbar = document.getElementById('navbar');
+const ulnavbar = document.getElementById('ulnavbar');
+
+document.onclick = function(e){
+    if(e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar'  && e.target.id !== 'ulnavbar'){
+        toggle.classList.remove('active');
+        navbar.classList.remove('active');
+        ulnavbar.classList.remove('active');
+    }
+}
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    navbar.classList.toggle('active');
+    ulnavbar.classList.toggle('active');
+}
