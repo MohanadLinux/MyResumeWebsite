8af4e11b6ad88bbcaae982cd59de4d456b5c3c2a
MohanadLinux <mohanadlinux@gmail.com>
2022-02-07 18:10:44 +0300

    Modifying Services section & Navbar

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 
 const header = document.getElementById('header');
 const toggle = document.getElementById('toggle');
+const toggle2 = document.getElementById('toggle2');
 const navbar = document.getElementById('navbar');
 const ulnavbar = document.getElementById('ulnavbar');
 
 document.onclick = function(e){
-    if(e.target.id !== 'header' && e.target.id !== 'toggle' 
-    && e.target.id !== 'navbar'  && e.target.id !== 'ulnavbar'){
+    if(e.target.id !== 'header' && e.target.id !== 'toggle'
+    && e.target.id !== 'toggle2' 
+    && e.target.id !== 'navbar'  
+    && e.target.id !== 'ulnavbar'){
         toggle.classList.remove('active');
+        toggle2.classList.remove('active');
         navbar.classList.remove('active');
         ulnavbar.classList.remove('active');
     }
@@ -15,6 +19,7 @@ document.onclick = function(e){
 
 toggle.onclick = function(){
     toggle.classList.toggle('active');
+    toggle2.classList.toggle('active');
     navbar.classList.toggle('active');
     ulnavbar.classList.toggle('active');
 }
